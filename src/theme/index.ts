b8974ac47c6240e96ca0924732721fd8b253e102
cristianelira <79479647+cristianelira@users.noreply.github.com>
2024-08-08 10:29:06 -0300

    style: add theme of app

---
+++ src/theme/index.ts
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+    green: '#91be3f',
+    blue_dark: '#15304d',
+    blue_light: '#23bbed',
+    blue: '#006baf'
+  }
+}

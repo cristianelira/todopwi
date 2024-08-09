86e0dccc5a6863962cda82ab0ca769bfdad6561b
cristianelira <79479647+cristianelira@users.noreply.github.com>
2024-08-09 15:23:07 -0300

    styles: alteração no tema padrão

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -5,6 +5,8 @@ export default {
     green: '#91be3f',
     blue_dark: '#15304d',
     blue_light: '#23bbed',
-    blue: '#006baf'
+    blue: '#006baf',
+    background: '#234f80',
+    gray: '#676c79'
   }
 }

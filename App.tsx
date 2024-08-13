4f748f5f82baa71701a2405c420a3efc641cbf21
cristianelira <79479647+cristianelira@users.noreply.github.com>
2024-08-13 19:55:41 -0300

    refactor: changing app alerts to toast message and add success message

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import theme from './src/theme'
 import { Home } from './src/screens/Home'
+import Toast from 'react-native-toast-message'
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <Home />
+      <Toast />
     </ThemeProvider>
   )
 }
